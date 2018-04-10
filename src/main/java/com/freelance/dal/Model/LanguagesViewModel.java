70fb80880b26ca4c743aeecb7130ae1b7fb8e4e4
abbos513 <abbos513@gmail.com>
2018-04-11 01:25:38 +0500

    List of languages displayed

---
+++ src/main/java/com/freelance/dal/Model/LanguagesViewModel.java
@@ -0,0 +1,11 @@
+package com.freelance.dal.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguagesViewModel {
+    List<String> languages = new ArrayList<String>();
+    List<String> languageLavels = new ArrayList<String>();
+
+
+}
